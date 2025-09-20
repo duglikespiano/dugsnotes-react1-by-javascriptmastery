8366ea42b38cdc67d9c687c97da4423b017cf562
SUHO PARK <duglikespiano@gmail.com>
2025-09-20 18:02:45 +0900

    FEATURE - HOME PAGE ADDED

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function Button() {
-	return <div>Button</div>;
+export default function Button({ color, bgColor, text, borderRadius }) {
+	return (
+		<button type="button" style={{ backgroundColor: bgColor, color, borderRadius }} className={`p-3 hover:drop-shadow-xl`}>
+			{text}
+		</button>
+	);
 }
