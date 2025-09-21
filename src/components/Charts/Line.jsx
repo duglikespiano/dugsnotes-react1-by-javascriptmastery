78be00439600754191a835a3597f33f2ea985cbd
SUHO PARK <duglikespiano@gmail.com>
2025-09-21 20:59:14 +0900

    FEATURE - LINE CHART PAGE ADDED

--- src/components/Charts/Line.jsx
+++ src/components/Charts/Line.jsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import {
+	ChartComponent,
+	SeriesCollectionDirective,
+	SeriesDirective,
+	Inject,
+	DateTime,
+	Legend,
+	Tooltip,
+	LineSeries,
+} from '@syncfusion/ej2-react-charts';
 
-export default function Line() {
-	return <div>Line</div>;
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+export default function LineChart() {
+	const { currentMode } = useStateContext();
+	return (
+		<ChartComponent
+			id="line-chart"
+			height="420px"
+			primaryXAxis={LinePrimaryXAxis}
+			primaryYAxis={LinePrimaryYAxis}
+			chartArea={{ border: { width: 0 } }}
+			tooltip={{ enable: true }}
+			background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+		>
+			<Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+			<SeriesCollectionDirective>
+				{lineCustomSeries.map((item, index) => (
+					<SeriesDirective key={index} {...item} />
+				))}
+			</SeriesCollectionDirective>
+		</ChartComponent>
+	);
 }
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { ChartsHeader, Header, LineChart } from '../../components';
 
 export default function Line() {
-	return <div>Line</div>;
+	return (
+		<div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+			<Header category="Chart" title="Inflation Rate" />
+			<div className="w-full ">
+				<LineChart />
+			</div>
+		</div>
+	);
 }
